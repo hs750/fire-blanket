63018f9fc80ff6db7885ba2bbafdd7c5b0821b14
Harrison <hs750@york.ac.uk>
2013-01-19 15:56:02 +0000

    Pair Tests
    Only added javaDoc to the test cases

--- src/tests/PairTest.java
+++ src/tests/PairTest.java
@@ -11,19 +11,28 @@ public class PairTest {
 	private Label testFirst = Label.temp;
 	private Double testSecond = 123.456;
 	private String testString  = testFirst + " " + testSecond;
+	
+	/**
+	 * Test whether the first item in the pair returns the correct value
+	 */
 	@Test
 	public void testFirst() {
 		Pair<Double> testPair = new Pair<Double>(testFirst, testSecond);
 		assertEquals(testFirst.toString(), testPair.first());
 				
 	}
-	
+	/**
+	 * Test whether the second item in the pair returns the correct value
+	 */
 	@Test
 	public void testSecond(){
 		Pair<Double> testPair = new Pair<Double>(testFirst, testSecond);
 		assertEquals(testSecond, testPair.second());
 	}
 	
+	/**
+	 * Test whether the pair returns the correct string relating to the values of its first and second values.
+	 */
 	@Test
 	public void testString(){
 		Pair<Double> testPair = new Pair<Double>(testFirst, testSecond);
