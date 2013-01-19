88855107b1bf582418051be89da04e1531764175
Andrei Simionescu <ansimionescu@gmail.com>
2013-01-19 14:06:58 +0000

    Modified anchovy.io.Parser#String parse(String) Added a new Test for io.Parser

--- src/anchovy/io/Parser.java
+++ src/anchovy/io/Parser.java
@@ -1,127 +1,104 @@
 package anchovy.io;
+
 import java.io.FileNotFoundException;
+import java.util.StringTokenizer;
+
 import anchovy.GameEngine;
 import anchovy.InfoPacket;
 import anchovy.Pair;
 import anchovy.Components.*;
-class Parser
-{
+
+class Parser {
 	private GameEngine engine;
-	public Parser(GameEngine Engine)
-	{
+
+	public Parser(GameEngine Engine) {
 		engine = Engine;
 	}
-	public String parseCommand(String componentName, String command)throws FileNotFoundException
-	{
+
+	public String parseCommand(String componentName, String command)
+			throws FileNotFoundException {
 		String result = new String();
-		Component component= engine.getPowerPlantComponent(componentName);
+		Component component = engine.getPowerPlantComponent(componentName);
 		InfoPacket i = new InfoPacket();
 
-		if(component != null)
-		{
+		if (component != null) {
 
-			if(component.getName().contains("Valve"))
-			{
-				//i.namedValues.add(new Pair<String>(Pair.Label.cNme, component.getName()));
-				if(command.equals("open"))
-				{
+			if (component.getName().contains("Valve")) {
+				// i.namedValues.add(new Pair<String>(Pair.Label.cNme,
+				// component.getName()));
+				if (command.equals("open")) {
 					i.namedValues.add(new Pair<Boolean>(Pair.Label.psit, true));
+				} else if (command.equals("close")) {
+					i.namedValues
+							.add(new Pair<Boolean>(Pair.Label.psit, false));
 				}
-				else if(command.equals("close"))
-				{
-					i.namedValues.add(new Pair<Boolean>(Pair.Label.psit, false));
-				}
-			}
-			else if(component.getName().contains("Pump"))
-			{
-				i.namedValues.add(new Pair<String>(Pair.Label.cNme, component.getName()));
-				if(command.equals("on"))
-				{
+			} else if (component.getName().contains("Pump")) {
+				i.namedValues.add(new Pair<String>(Pair.Label.cNme, component
+						.getName()));
+				if (command.equals("on")) {
 					i.namedValues.add(new Pair<Boolean>(Pair.Label.psit, true));
-				}
-				else if(command.equals("off"))
-				{
-					i.namedValues.add(new Pair<Boolean>(Pair.Label.psit, false));
+				} else if (command.equals("off")) {
+					i.namedValues
+							.add(new Pair<Boolean>(Pair.Label.psit, false));
 				}
 			}
 
-			else if(component.getName().contains("Control rods"))
-			{
-				i.namedValues.add(new Pair<String>(Pair.Label.cNme, component.getName()));
-				if(command.equals("lower"))
-				{
+			else if (component.getName().contains("Control rods")) {
+				i.namedValues.add(new Pair<String>(Pair.Label.cNme, component
+						.getName()));
+				if (command.equals("lower")) {
 					i.namedValues.add(new Pair<Boolean>(Pair.Label.psit, true));
+				} else if (command.equals("raise")) {
+					i.namedValues
+							.add(new Pair<Boolean>(Pair.Label.psit, false));
 				}
-				else if(command.equals("raise"))
-				{
-					i.namedValues.add(new Pair<Boolean>(Pair.Label.psit, false));
-				}
-			} else if(command.equals("repair"))
-			{
+			} else if (command.equals("repair")) {
 				engine.repair(component);
 			}
-			try{
+			try {
 				component.takeInfo(i);
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			catch(Exception e) 
-			{ e.printStackTrace(); }
+		} else if (componentName.contains("save as")) {
+			return engine.saveGameState(engine.getAllComponentInfo(), command);
+		} else if (componentName.equals("save")) {
+
+			return engine.save(engine.getAllComponentInfo());
+		} else if (componentName.contains("load")) {
+			engine.readfile(command);
+			engine.updateInterfaceComponents(engine.getAllComponentInfo());
+
+		} else if (componentName.equals("show saves")) {
+			return engine.findAvailableSaves();
 		}
-			else if(componentName.contains("save as"))
-			{
-				return engine.saveGameState(engine.getAllComponentInfo(), command);
-			}
-			else if(componentName.equals("save"))
-			{
 
-				return engine.save(engine.getAllComponentInfo());
-			}
-			else if(componentName.contains("load"))
-			{
-				engine.readfile(command);
-				engine.updateInterfaceComponents(engine.getAllComponentInfo());
+		else
 
-			}
-			else if(componentName.equals("show saves"))
-			{
-				return engine.findAvailableSaves();
-			}
-			
-			
-			else 
-			
-			{
-				result = "Component " + componentName + " does not exist";
-			}
-		
-	
+		{
+			result = "Component " + componentName + " does not exist";
+		}
+
 		engine.updateInterfaceComponents(engine.getAllComponentInfo());
 		return result;
 	}
-	public String parse(String text)throws FileNotFoundException
-	{	
+
+	public String parse(String in) throws FileNotFoundException {
+		if (in.length() == 0 || in.contains(" ") == false)
+			return "";
 		
-		if(text.length() != 0)
-		{
+		String inLower = in.toLowerCase();
 		
-			//String lowerCase = text.toLowerCase(); //Convert string to lower case to avoid case mismatches
-			String lowerCase= text;
-			if(lowerCase.contains(" "))
-			{
-				if(lowerCase.equals("show saves"))
-					return parseCommand(lowerCase, lowerCase);
-			String s= lowerCase.substring(0, lowerCase.lastIndexOf(' '));
-			String i= lowerCase.substring(lowerCase.lastIndexOf(' ') + 1, lowerCase.length());
-			
-			
-			return parseCommand(s,i);
-			}
-			else if(lowerCase.equals("save"))
-			{
-				return parseCommand(lowerCase,lowerCase);
-			}
-			else return "";
+		if (inLower.equals("save")) {
+			return parseCommand(inLower, inLower);
 		}
 		
-		else return "";
+		if (inLower.equals("show saves")) {
+			return parseCommand(inLower, inLower);
+		}
+		
+		StringTokenizer st = new StringTokenizer(inLower, " \t,;'-()");
+		
+		return parseCommand(st.nextToken(), st.nextToken());
 	}
 }
---
+++ src/tests/ParserTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+/**
+ * 
+ * @author andrei
+ */
+public class ParserTest {
+	
+	@BeforeClass
+	public void initParser() {
+		
+	}
+	
+	/**
+	 * Test method for {@link anchovy.io.Parser#parseCommand(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testParseCommand() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link anchovy.io.Parser#parse(java.lang.String)}.
+	 */
+	@Test
+	public void testParse() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
