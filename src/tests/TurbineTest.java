55a8e240d759f3e0ffc0b8fa05887792a00ee2dc
Harrison <hs750@york.ac.uk>
2013-01-19 16:23:42 +0000

    Turbine Tests

--- src/tests/TurbineTest.java
+++ src/tests/TurbineTest.java
@@ -6,12 +6,18 @@ import org.junit.Test;
 
 import anchovy.InfoPacket;
 import anchovy.Pair;
+import anchovy.Components.Pump;
 import anchovy.Components.Turbine;
 import anchovy.Pair.Label;
 
 public class TurbineTest {
 	private Turbine turbine1;
 	private InfoPacket info;
+	
+	/**
+	 * Setup a new turbine and an info packet for use in the tests.
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 		turbine1 = new Turbine("Turbine 1");
@@ -20,6 +26,9 @@ public class TurbineTest {
 		info.namedValues.add(new Pair<Double>(Label.RPMs, 30.0));
 	}
 
+	/**
+	 * Test whether a turbine takes info correctly
+	 */
 	@Test
 	public void testTakeInfo() {
 		try {
@@ -32,5 +41,32 @@ public class TurbineTest {
 		assertTrue(turbine1.getRPM() == 30.0);
 	}
 	
+	/**
+	 * Test whether a turbine correctly returns its info.
+	 */
+	@Test
+	public void testGetInfo(){
+		turbine1.setRPM(200.0);
+		
+		InfoPacket t1I = turbine1.getInfo();
+		assertTrue(t1I.namedValues.contains(new Pair<Double>(Label.RPMs, 200.0)));
+		assertTrue(t1I.namedValues.contains(new Pair<String>(Label.cNme, "Turbine 1")));
+	}
+	
+	/**
+	 * Test whether the turbine calculates correctly.
+	 */
+	@Test
+	public void testCalculate(){
+		Pump p = new Pump("Pump 1");
+		p.setOuputFlowRate(100);
+		
+		turbine1.connectToInput(p);
+		turbine1.calculate();
+		
+		assertTrue(turbine1.getRPM() == p.getOutputFlowRate()/turbine1.getRPMRatio());
+		assertTrue(turbine1.getOutputFlowRate() == p.getOutputFlowRate());
+		
+	}
 
 }
