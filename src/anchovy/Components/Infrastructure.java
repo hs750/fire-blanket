110ba4621cf8ba1fa906a9d69f9fe9455b6484b9
Harrison <hs750@york.ac.uk>
2013-01-19 10:20:46 +0000

    Infrastructure JavaDoc

--- src/anchovy/Components/Infrastructure.java
+++ src/anchovy/Components/Infrastructure.java
@@ -8,31 +8,46 @@ import anchovy.Pair;
 import anchovy.Pair.Label;
 
 public class Infrastructure extends Component {
-	//need to give electrisityneeded a value somewhere
+	/**
+	 * The electricity that is needed to power this piece of infrastructure.
+	 */
 	double electrisityneeded;
 	boolean failed = false;
+	
+	/**
+	 * @see anchovy.Components.Component#Component(String)
+	 */
 	public Infrastructure(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
 	}
-
+	/**
+	 * @see anchovy.Components.Component#Component(String, InfoPacket)
+	 */
 	public Infrastructure(String name, InfoPacket info) {
 		super(name, info);
 	}
 
 
-
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public InfoPacket getInfo() {
 		InfoPacket info = super.getSuperInfo();
 		return info;
 	}
-
+	/** 
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void calculate() {
 		calculateOutputFlowRate();
 	}
-
+	
+	/** 
+	 * {@inheritDoc}
+	 * The output flow rate of an infrastructure component is the total amount of electricity flowing into the component, minus the electricity that it itself needs to function.
+	 */
 	@Override
 	protected double calculateOutputFlowRate() {
 		double electrisityGenerated = 0;
@@ -48,13 +63,18 @@ public class Infrastructure extends Component {
 			}
 		return electrisityGenerated;
 	}
-
+	/** 
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void takeInfo(InfoPacket info) throws Exception {
 		super.takeSuperInfo(info);
 
 	}
-
+	/**
+	 * {@inheritDoc}
+	 * Infrastructure components fail when they are receiving less electricity than they need.
+	 */
 	@Override
 	protected boolean calculateFailed() {
 		if (electrisityneeded > this.getOutputFlowRate()){
