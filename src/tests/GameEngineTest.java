f43b15b54b5a2c4e7c1b3d77d71d165fb906c496
Harrison <hs750@york.ac.uk>
2013-01-20 13:23:51 +0000

    made it so tests on creating files delete file at the end (other wise the test would be poinless)

--- src/tests/GameEngineTest.java
+++ src/tests/GameEngineTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -163,6 +164,9 @@ public class GameEngineTest {
 			assertTrue(false);
 		}
 		assertTrue(allCompInfo.equals(gameEngine.getAllComponentInfo()));
+		
+		File f = new File("saves/test.fg");
+		f.delete();
 	}
 	/**
 	 * Test whether the the game can search for and display the location of a particular game save.
@@ -170,9 +174,14 @@ public class GameEngineTest {
 	@Test
 	public void testShowAvailableSaves()
 	{
+		Valve v = (Valve) gameEngine.getPowerPlantComponent("Valve 1");
+		v.setPosition(false);
 		gameEngine.saveGameState(gameEngine.getAllComponentInfo(), "show saves");
 		
 		
 		assertTrue(gameEngine.findAvailableSaves().contains("show saves"));
+		
+		File f = new File("saves/show saves.fg");
+		f.delete();
 	}
 }
