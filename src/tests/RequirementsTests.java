ce3d0eea402aad46208133e0547a3a09bd312296
Harrison <hs750@york.ac.uk>
2013-01-21 17:51:51 +0000

    Requirement test TU02 (doesnt currently work, awaiting change to reactor)

--- src/tests/RequirementsTests.java
+++ src/tests/RequirementsTests.java
@@ -20,7 +20,7 @@ public class RequirementsTests {
 	 * While the turbine is turning the generator, the total amount of power produced by the power plant will increase proportionally to the amount of steam flowing round the system. 
 	 * Test implemented by creating a very simple power plant configuration of a valve, turbine and generator.
 	 * The valves output flow rate is set to 100 then the turbine and generator calculate(), getting the initial value of the electricity being output.
-	 * The valves output flow rate is then set to 200 and calculates are preformed again. 
+	 * The valves output flow rate is then set to 200 and calculates are performed again. 
 	 * Then it is asserted that the output from the generator increased (due to the flow rate entering the turbine increasing thus increasing its RPM, thus generating more electricity). 
 	 * 
 	 */
@@ -49,6 +49,45 @@ public class RequirementsTests {
 		generator.calculate();
 		
 		assertTrue("Original: " + outputOfGenerator + ". New: " + generator.getOutputFlowRate(), outputOfGenerator < generator.getOutputFlowRate());
+	}
+	
+	@Test
+	public void TU01_SF09(){
+		Reactor reactor = new Reactor("Reactor");
+		Valve valve = new Valve("Valve");
+		
+		double[] reactorOutputFlow = {0,0,0};
+		
+		reactor.connectToInput(valve);
+		reactor.connectToOutput(valve);
+		valve.setOuputFlowRate(50);
+		
+		reactor.setControlRodLevel(50);
+		reactor.setAmount(50);
+		reactor.setTemperature(125);
+		reactor.setWaterLevel(50);
+		reactor.calculate();
+	
+		reactorOutputFlow[0] = reactor.getOutputFlowRate();
+		
+		reactor.setTemperature(125);
+		reactor.setControlRodLevel(50);
+		reactor.setWaterLevel(50);
+		reactor.setAmount(50);
+		reactor.calculate();
+	
+		reactorOutputFlow[1] = reactor.getOutputFlowRate();
+		
+		reactor.setTemperature(125);
+		reactor.setControlRodLevel(50);
+		reactor.setWaterLevel(50);
+		reactor.setAmount(50);
+		reactor.calculate();
+	
+		reactorOutputFlow[2] = reactor.getOutputFlowRate();
+		
+		assertTrue("" + reactorOutputFlow[0] + " " + reactorOutputFlow[1] + " " +reactorOutputFlow[2], reactorOutputFlow[0] < reactorOutputFlow[1] && reactorOutputFlow[2] == 0);
+		
 	}
 
 }
